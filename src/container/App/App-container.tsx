c0db637a8fcf95ca469a4d042a5a1c2bc4a80398
Prajun Trital <prajuncs@gmail.com>
2023-01-15 12:15:39 -0500

    cleaning

---
+++ src/container/App/App-container.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import AppView from './App-view';
+
+const App: React.FC = () => (
+    <AppView />
+);
+
+export default App;
---
+++ src/container/App/App-view.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AppView: React.FC = () => {
+
+  return (
+    <>
+        HELLO
+    </>
+  );
+};
+
+export default AppView;
