fe883df1eea86b767fe6f9f1a3afcafc08fc6486
Prajun Trital <prajuncs@gmail.com>
2023-01-15 14:06:29 -0500

    containers

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './container'
+import App from './containers'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
