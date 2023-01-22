8eb0061d23e1b7c5f9cdcd67e08406170a76cb32
Prajun Trital <prajuncs@gmail.com>
2023-01-21 21:30:42 -0500

    responsive design

--- src/components/SideBar/SideBar-view.tsx
+++ src/components/SideBar/SideBar-view.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
+import {
+  useMediaQuery,
+  useTheme,
+} from '@mui/material';
 import SideBarDesktop from './Desktop/SideBarDesktop-view';
 import SideBarMobile from './Mobile/SideBarMobile-view';
 
 const SidebarView: React.FC = () => {
-  const isMobile = false;
+  const theme = useTheme();
+  const isDesktop = useMediaQuery(theme.breakpoints.up('md'));
 
-  if (isMobile) {
+  if (!isDesktop) {
     return (
       <SideBarMobile />
     );
--- src/containers/App/App-view.tsx
+++ src/containers/App/App-view.tsx
@@ -1,14 +1,21 @@
 import React from 'react';
 import DashboardApp from '../Dashboard/DashboardApp';
 import { Route, Routes } from 'react-router-dom';
+import {
+  ThemeProvider,
+  createTheme,
+} from '@mui/material/styles';
 
+const theme = createTheme();
 
 const AppView: React.FC = () => {
-
   return (
-    <Routes>
-      <Route path="/*" element={<DashboardApp />} />
-    </Routes>
+    <ThemeProvider theme={theme}>
+      <Routes>
+        <Route path="/*" element={<DashboardApp />} />
+      </Routes>
+    </ThemeProvider>
+
   );
 };
 
